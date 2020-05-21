c4bd503086cf29ae529ebbba0ba4db502bfc6dfb
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-21 18:06:16 +0200

    Update LogginController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
@@ -1,13 +1,16 @@
 package org.example.Controller;
 
 import org.example.Entities.Profesores;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -21,32 +24,34 @@ public class LogginController {
     DataSource dataSource;
     Connection connection;
     Profesores profesores;
+    String Email_Usuario;
+    String Clave_Usuario;
     @GetMapping("/")
-    public String InicioSesion(HttpServletRequest request, Model model){
+    public String InicioSesion(HttpServletRequest request, Model model) {
         return "Login";
     }
 
     @PostMapping("/Comprobar")
-    public String ComprobarUsuario(HttpServletRequest request,Model model)throws SQLException {
+    public String ComprobarUsuario(HttpServletRequest request, Model model) throws SQLException {
 
 
-        String Email_Usuario=request.getParameter("Email_Acceso");
-        String Clave_Usuario=request.getParameter("Clave_Acceso");
-        connection=dataSource.getConnection();
-        String Profesor="Select * from profesores where Email=? and Password=md5(?)";
-        PreparedStatement preparedStatement=connection.prepareStatement(Profesor);
-        preparedStatement.setString(1,Email_Usuario);
-        preparedStatement.setString(2,Clave_Usuario);
+         Email_Usuario= request.getParameter("Email_Acceso");
+         Clave_Usuario = request.getParameter("Clave_Acceso");
+        connection = dataSource.getConnection();
+        String Profesor = "Select * from profesores where Email=? and Password=md5(?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(Profesor);
+        preparedStatement.setString(1, Email_Usuario);
+        preparedStatement.setString(2, Clave_Usuario);
 
         ResultSet resultSet;
-        resultSet=preparedStatement.executeQuery();
-        if(resultSet.next()){
-            profesores=new Profesores();
+        resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            profesores = new Profesores();
             profesores.setUsuarioProfesor(resultSet.getString("Usuario"));
             profesores.setIdProfesor(resultSet.getInt("ProfesorId"));
-            HttpSession session=request.getSession();
-            session.setAttribute("UsuarioConectado",profesores);
-
+            HttpSession session = request.getSession();
+            session.setAttribute("UsuarioConectado", profesores);
+            model.addAttribute("UsuarioConectado",profesores);
             return "redirect:/homeProfesores";
         }
 
@@ -54,4 +59,22 @@ public class LogginController {
         return "Login";
     }
 
+
+    @GetMapping("/logout")
+    public String Cerrar_Sesion(HttpServletRequest request, HttpServletResponse response, Model model) {
+        Cookie[] cookies = request.getCookies();
+        HttpSession session=request.getSession();
+        model.asMap().clear();
+        session.invalidate();
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(0);
+            cookie.setValue(null);
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
+
+        }
+        return "Login";
+
+    }
 }
