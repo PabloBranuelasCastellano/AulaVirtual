f7e8d8fdd357b51083c3e9720a906fecab386e93
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-21 15:19:29 +0200

    Create ProfesorController.java

---
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -0,0 +1,25 @@
+package org.example.Controller;
+
+import org.example.Entities.Profesores;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ProfesorController {
+
+    @Autowired
+    LogginController logginController;
+    Profesores profesores;
+
+    @GetMapping("/homeProfesores")
+    public String PanelProfesor(HttpServletRequest request, Model model){
+        profesores=logginController.profesores;
+        System.out.println(profesores.getIdProfesor());
+        System.out.println(profesores.getUsuarioProfesor());
+        return "panelprofesores";
+    }
+}
