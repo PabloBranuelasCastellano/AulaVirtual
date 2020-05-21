e13e53834f94c20fbc9460f5f365f543326d0b1f
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-21 14:31:42 +0200

    Update LogginController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
@@ -5,11 +5,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Controller
 public class LogginController {
     @GetMapping("/")
     public String InicioSesion(HttpServletRequest request, Model model){
+        return "Login";
+    }
+
+    @GetMapping("/Comprobar")
+    public String ComprobarUsuario(HttpServletRequest request,Model model)throws SQLException {
+        DataSource dataSource=null;
+        Connection connection=null;
+        String Email_Usuario=request.getParameter("Email_Acceso");
+        String Clave_Usuario=request.getParameter("Clave_Acceso");
+        connection=dataSource.getConnection();
+        String Profesor="Select * from profesores where Email=? and Password=md5(?)";
+        PreparedStatement preparedStatement=connection.prepareStatement(Profesor);
+        preparedStatement.setString(1,Email_Usuario);
+        preparedStatement.setString(2,Clave_Usuario);
+        ResultSet resultSet;
+        resultSet=preparedStatement.executeQuery();
+        if(resultSet.next()){
+            System.out.println("Usuario Encontrado");
+        }
+
         return "Login";
     }
 }
