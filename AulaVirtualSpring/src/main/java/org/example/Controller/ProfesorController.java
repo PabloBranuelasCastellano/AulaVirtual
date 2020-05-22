e1795b2db629a43dca821a77780fa27454385527
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-22 08:51:35 +0200

    Update ProfesorController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -14,6 +14,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProfesorController {
@@ -41,13 +43,16 @@ public class ProfesorController {
         String VerGrupos="select g.GrupoId, ca.Denominacion as AnioEscolar ,G.Nombre as NombreGrupo ,M.Nombre as Asignatura ,N.Denominacion as NivelEducativo ,P.Usuario as Nombre_Profesor from grupos g,materias m ,niveles n ,profesores p, cursosacademicos ca where(g.MateriaId=M.MateriaId and g.NivelId =n.NivelId and g.ProfesorId =P.ProfesorId and g.CursoAcademicoId =ca.CursoAcademicoId  and ca.EsActivo =true and P.ProfesorId=? )";
         PreparedStatement preparedStatement=connection.prepareStatement(VerGrupos);
         preparedStatement.setInt(1,logginController.profesores.getIdProfesor());
-        System.out.println(logginController.profesores.getIdProfesor());
+        //System.out.println(logginController.profesores.getIdProfesor());
         ResultSet resultSet=preparedStatement.executeQuery();
+        List<Grupos>gruposMateria;
         while(resultSet.next()){
-            System.out.println(resultSet.getString("Denominacion"));
-            System.out.println(resultSet.getString("Grupo"));
-            System.out.println(resultSet.getString("Nombre"));
-            System.out.println(resultSet.getString("Usuario"));
+            gruposMateria=new ArrayList<>();
+
+            grupos.setCursoAcademicoGrupo(resultSet.getString(2));
+            System.out.println(resultSet.getString(3));
+            System.out.println(resultSet.getString(4));
+            System.out.println(resultSet.getString(6));
         }
 
         return "panelprofesores";
