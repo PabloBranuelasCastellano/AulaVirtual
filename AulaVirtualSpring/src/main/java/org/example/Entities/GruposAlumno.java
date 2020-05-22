622585a6be1d669fdd91c62156c63290ea0995bd
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-22 11:45:53 +0200

    Update GruposAlumno.java

--- AulaVirtualSpring/src/main/java/org/example/Entities/GruposAlumno.java
+++ AulaVirtualSpring/src/main/java/org/example/Entities/GruposAlumno.java
@@ -1,4 +1,40 @@
 package org.example.Entities;
 
 public class GruposAlumno {
+    private int IdAlumno;
+    private String nombreAlumno;
+    private String primerApellidoAlumno;
+    private String segundoApellidoAlumno;
+
+    public int getIdAlumno() {
+        return IdAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        IdAlumno = idAlumno;
+    }
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public void setNombreAlumno(String nombreAlumno) {
+        this.nombreAlumno = nombreAlumno;
+    }
+
+    public String getPrimerApellidoAlumno() {
+        return primerApellidoAlumno;
+    }
+
+    public void setPrimerApellidoAlumno(String primerApellidoAlumno) {
+        this.primerApellidoAlumno = primerApellidoAlumno;
+    }
+
+    public String getSegundoApellidoAlumno() {
+        return segundoApellidoAlumno;
+    }
+
+    public void setSegundoApellidoAlumno(String segundoApellidoAlumno) {
+        this.segundoApellidoAlumno = segundoApellidoAlumno;
+    }
 }
