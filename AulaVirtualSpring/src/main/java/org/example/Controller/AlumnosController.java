1e1fc69f11b24317e7498ff275bb8203e6ffd480
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-22 14:24:41 +0200

    Update AlumnosController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/AlumnosController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/AlumnosController.java
@@ -2,17 +2,19 @@ package org.example.Controller;
 
 import org.example.Entities.Alumnos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
-
+@Controller
 public class AlumnosController {
     @Autowired
     LogginController logginController;
     Alumnos alumnos;
+
     @GetMapping("/homeAlumnos")
     public  String PanelAlumnos(HttpServletRequest request, Model model)throws SQLException{
         System.out.println("Pagina de Alumnos");
