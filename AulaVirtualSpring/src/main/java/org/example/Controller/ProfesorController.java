11fbb5ff8ffe85e9023f6a1cc791f86853594f17
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-22 08:19:31 +0200

    Update ProfesorController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,11 +18,15 @@ public class ProfesorController {
 
     @GetMapping("/homeProfesores")
     public String PanelProfesor(HttpServletRequest request, Model model){
+
         profesores=logginController.profesores;
         System.out.println(profesores.getIdProfesor());
         System.out.println(profesores.getUsuarioProfesor());
         return "panelprofesores";
     }
+
+
+
 
 
 
