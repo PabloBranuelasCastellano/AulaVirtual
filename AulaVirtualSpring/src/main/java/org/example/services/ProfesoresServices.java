171a739555b8f6ce6d4814c30bcae2c3fecae736
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 11:23:05 +0200

    Update ProfesoresServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
@@ -5,9 +5,12 @@ import org.example.Entities.GruposAlumno;
 import org.example.Entities.Profesores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 @Service
 public class ProfesoresServices {
@@ -18,4 +21,12 @@ public class ProfesoresServices {
     GruposAlumno gruposAlumno;
     DataSource dataSource;
     Connection connection;
+
+    public String PanelProfesor(HttpServletRequest request, Model model)throws SQLException {
+
+        profesores=loginServices.getProfesores();
+        /*System.out.println(profesores.getIdProfesor());
+        System.out.println(profesores.getUsuarioProfesor());*/
+        return "panelprofesores";
+    }
 }
