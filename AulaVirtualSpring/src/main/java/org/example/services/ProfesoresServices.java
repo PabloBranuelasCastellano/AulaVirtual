4b6a13c2cfa650095d0d774f33bfde9994229494
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 11:50:26 +0200

    Update ProfesoresServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
@@ -10,7 +10,11 @@ import org.springframework.ui.Model;
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProfesoresServices {
@@ -19,14 +23,56 @@ public class ProfesoresServices {
     Profesores profesores;
     Grupos grupos;
     GruposAlumno gruposAlumno;
-    DataSource dataSource;
-    Connection connection;
+
+    @Autowired
+    DataSource dataSource=null;
+    Connection connection=null;
 
     public String PanelProfesor(HttpServletRequest request, Model model)throws SQLException {
 
         profesores=loginServices.getProfesores();
         /*System.out.println(profesores.getIdProfesor());
         System.out.println(profesores.getUsuarioProfesor());*/
+        return MostrarGrupos(request, model);
+    }
+    public String MostrarGrupos(HttpServletRequest request,Model model)throws SQLException {
+        connection=dataSource.getConnection();
+
+        String VerGrupos="select g.GrupoId, ca.Denominacion as AnioEscolar ,G.Nombre as NombreGrupo ,M.Nombre as Asignatura ,N.Denominacion as NivelEducativo ,P.Usuario as Nombre_Profesor from grupos g,materias m ,niveles n ,profesores p, cursosacademicos ca where(g.MateriaId=M.MateriaId and g.NivelId =n.NivelId and g.ProfesorId =P.ProfesorId and g.CursoAcademicoId =ca.CursoAcademicoId  and ca.EsActivo =true and P.ProfesorId=?)";
+
+        PreparedStatement preparedStatement=connection.prepareStatement(VerGrupos);
+        preparedStatement.setInt(1,profesores.getIdProfesor());
+        //System.out.println("Consulta preparada");
+
+        //System.out.println(logginController.profesores.getIdProfesor());
+        ResultSet resultSet=preparedStatement.executeQuery();
+        List<Grupos> gruposMateria=new ArrayList<>();
+        //System.out.println("Lanzamos la consulta");
+
+        while(resultSet.next()){
+            grupos=new Grupos();
+            System.out.print(resultSet.getInt(1)+" ");
+            grupos.setIdGrupo(resultSet.getInt(1));
+            System.out.print(resultSet.getString(2)+" ");
+            grupos.setCursoAcademicoGrupo(resultSet.getString(2));
+            System.out.print(resultSet.getString(3)+" ");
+            grupos.setNombreGrupo(resultSet.getString(3));
+            grupos.setNombreMateria(resultSet.getString(4));
+            System.out.print(resultSet.getString(4)+" ");
+            System.out.print(resultSet.getString(5)+" ");
+            grupos.setNivelGrupo(resultSet.getString(5));
+            System.out.print(resultSet.getString(6)+" ");
+            grupos.setProfesorGrupo(resultSet.getString(6));
+            System.out.println();
+            gruposMateria.add(grupos);
+
+        }
+        //System.out.println("Recorremos el bucle .guardamos los valores");
+
+
+        model.addAttribute("Grupos_Materia",gruposMateria);
+
+        System.out.println("LLegamos al final y funciona");
         return "panelprofesores";
     }
 }
