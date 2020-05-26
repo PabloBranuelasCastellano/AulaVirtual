8187bb61eb80fb9fbc60ec9bc25a850123f929b3
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 13:56:50 +0200

    Update AlumnosServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
@@ -8,16 +8,20 @@ import org.springframework.ui.Model;
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 @Service
 public class AlumnosServices {
     @Autowired
     LoginServices loginServices;
-    DataSource dataSource=null;
-    Connection connection=null;
+
     Alumnos alumnos;
 
+
+    @Autowired
+    DataSource dataSource=null;
+    Connection connection=null;
     public  String PanelAlumnos(HttpServletRequest request, Model model)throws SQLException {
         alumnos=loginServices.getAlumnos();
         return MateriasAlumno(request, model);
@@ -25,6 +29,10 @@ public class AlumnosServices {
 
     public String MateriasAlumno(HttpServletRequest request,Model model)throws SQLException{
         connection=dataSource.getConnection();
+        String Materias_alumno="select distinct M.Nombre,m.MateriaId,m.Nombre ,n.Denominacion,n.NivelId  from temas t,niveles n,profesores p,materias m where (t.NivelId =n.NivelId and p.ProfesorId =t.ProfesorId and m.EsActiva =true and P.ProfesorId=?)";
+        PreparedStatement preparedStatement=connection.prepareStatement(Materias_alumno);
+        preparedStatement.setInt(1,loginServices.getAlumnos().getIdAlumno());
+        System.out.println("Consulta preparada .El Id del alumno es "+loginServices.getAlumnos().getIdAlumno());
         return "panelalumnos";
     }
 }
