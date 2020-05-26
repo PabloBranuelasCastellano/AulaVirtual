0cd3ded125fd4eb65cf45212312ece9a5bfec590
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 12:33:14 +0200

    Update ProfesorController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -42,39 +42,7 @@ public class ProfesorController {
     public String PanelProfesor(HttpServletRequest request, Model model)throws SQLException{
         return profesoresServices.PanelProfesor(request, model);
     }
-    /*
-    public String MostrarGrupos(HttpServletRequest request,Model model)throws SQLException {
-        connection=dataSource.getConnection();
-        String VerGrupos="select g.GrupoId, ca.Denominacion as AnioEscolar ,G.Nombre as NombreGrupo ,M.Nombre as Asignatura ,N.Denominacion as NivelEducativo ,P.Usuario as Nombre_Profesor from grupos g,materias m ,niveles n ,profesores p, cursosacademicos ca where(g.MateriaId=M.MateriaId and g.NivelId =n.NivelId and g.ProfesorId =P.ProfesorId and g.CursoAcademicoId =ca.CursoAcademicoId  and ca.EsActivo =true and P.ProfesorId=?)";
-        PreparedStatement preparedStatement=connection.prepareStatement(VerGrupos);
-        preparedStatement.setInt(1,profesores.getIdProfesor());
-        //System.out.println(logginController.profesores.getIdProfesor());
-        ResultSet resultSet=preparedStatement.executeQuery();
-        List<Grupos>gruposMateria=new ArrayList<>();
-        while(resultSet.next()){
-            grupos=new Grupos();
-            System.out.print(resultSet.getInt(1)+" ");
-            grupos.setIdGrupo(resultSet.getInt(1));
-            System.out.print(resultSet.getString(2)+" ");
-            grupos.setCursoAcademicoGrupo(resultSet.getString(2));
-            System.out.print(resultSet.getString(3)+" ");
-            grupos.setNombreGrupo(resultSet.getString(3));
-            grupos.setNombreMateria(resultSet.getString(4));
-            System.out.print(resultSet.getString(4)+" ");
-            System.out.print(resultSet.getString(5)+" ");
-            grupos.setNivelGrupo(resultSet.getString(5));
-            System.out.print(resultSet.getString(6)+" ");
-            grupos.setProfesorGrupo(resultSet.getString(6));
-            System.out.println();
-            gruposMateria.add(grupos);
 
-        }
-        model.addAttribute("Grupos_Materia",gruposMateria);
-
-
-
-        return "panelprofesores";
-    }*/
 
     @GetMapping("/VerAlumnos/{GrupoId}")
     public String AlumnosGrupos(HttpServletRequest request,Model model,@PathVariable int GrupoId)throws SQLException{
