6575efa8905f7e7a1a5068e3751413603552eea3
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 11:20:40 +0200

    Update ProfesoresServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/ProfesoresServices.java
@@ -1,8 +1,21 @@
 package org.example.services;
 
+import org.example.Entities.Grupos;
+import org.example.Entities.GruposAlumno;
+import org.example.Entities.Profesores;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Service
 public class ProfesoresServices {
-    
+    @Autowired
+    LoginServices loginServices;
+    Profesores profesores;
+    Grupos grupos;
+    GruposAlumno gruposAlumno;
+    DataSource dataSource;
+    Connection connection;
 }
