1f545207da0bd8df1d73c449b146b3611bd72b88
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-26 13:33:42 +0200

    Update AlumnosServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
@@ -1,7 +1,25 @@
 package org.example.services;
 
+import org.example.Entities.Alumnos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Service
 public class AlumnosServices {
+    @Autowired
+    LoginServices loginServices;
+    DataSource dataSource=null;
+    Connection connection=null;
+    Alumnos alumnos;
+
+    public  String PanelAlumnos(HttpServletRequest request, Model model)throws SQLException {
+        alumnos=loginServices.getAlumnos();
+        return "panelalumnos";
+    }
 }
