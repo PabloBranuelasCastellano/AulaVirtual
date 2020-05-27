1c3ec14c25362da7105b34f2d885190746143048
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-27 11:15:06 +0200

    Create Materias.java

---
+++ AulaVirtualSpring/src/main/java/org/example/Entities/Materias.java
@@ -0,0 +1,22 @@
+package org.example.Entities;
+
+public class Materias {
+    private int MateriaId;
+    private String NombreMateria;
+
+    public int getMateriaId() {
+        return MateriaId;
+    }
+
+    public void setMateriaId(int materiaId) {
+        MateriaId = materiaId;
+    }
+
+    public String getNombreMateria() {
+        return NombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        NombreMateria = nombreMateria;
+    }
+}
