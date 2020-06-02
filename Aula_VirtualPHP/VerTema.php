ca336aebe8e205051fd397b8b326cd462e49e0b0
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-02 10:41:26 +0200

    Update VerTema.php

--- Aula_VirtualPHP/VerTema.php
+++ Aula_VirtualPHP/VerTema.php
@@ -7,7 +7,7 @@
 		$Id_Profesor=$_SESSION["ProfesorId"];
 		$Id_Materia=$_GET["Id_Activo"];
 		// $Id_Materia=$_SESSION[$Id_Materia];
-		echo($Id_Profesor."&nbsp;&nbsp;El id de la Materia es ".$Id_Materia);
+		// echo($Id_Profesor."&nbsp;&nbsp;El id de la Materia es ".$Id_Materia);
 		if($Usuario_Actual=="" or $Usuario_Actual==null){
 			header("Location:index.php");
 		}
@@ -23,14 +23,14 @@
 	</head>
 	<body>
 		<?php
-			$Ver_Temas="select distinct t.titulo,m.Nombre,t.TemaId from temas t, profesores p ,niveles n,materias m where(t.materiaId=m.MateriaId and n.nivelId=t.nivelId and t.profesorId=$Id_Profesor and t.materiaId=$Id_Materia)"; 
+			$Ver_Temas="select distinct t.titulo,m.Nombre,t.TemaId,n.NivelId from temas t, profesores p ,niveles n,materias m where(t.materiaId=m.MateriaId and n.nivelId=t.nivelId and t.profesorId=$Id_Profesor and t.materiaId=$Id_Materia)"; 
 			$Temario=mysqli_query($conexion,$Ver_Temas);
 			$nfilas=mysqli_num_rows($Temario);
 			if($nfilas>0){
 				echo("<ul class='list-group'>");
 				while($fila=mysqli_fetch_array($Temario)){
 					echo(
-						"<li class='list-group-item list-group-item-action list-group-item-primary'>".$fila['titulo']."<a class='btn btn-primary' data-toggle='collapse' href='#Tema".strval($fila["TemaId"])."' role='button' aria-expanded='false' aria-controls='multiCollapseExample1' style='margin-left:50%;'>Ver Tema</a><a class='btn btn-primary' role='button' href='Agregarpunto.php?Id_Tema=".strval($fila["TemaId"])."' role='button' aria-expanded='false' aria-controls='multiCollapseExample1' style='margin-left:10%;'>Añadir Puntos</a>
+						"<li class='list-group-item list-group-item-action list-group-item-primary'>".$fila['titulo']."<a class='btn btn-primary' data-toggle='collapse' href='#Tema".strval($fila["TemaId"])."' role='button' aria-expanded='false' aria-controls='multiCollapseExample1' style='margin-left:10%;'>Ver Tema</a>&nbsp;&nbsp;<a class='btn btn-primary' role='button' href='Agregarpunto.php?Id_Tema=".strval($fila["TemaId"])."' role='button' aria-expanded='false' aria-controls='multiCollapseExample1' style='margin-left:30%;'>Añadir Puntos</a> &nbsp;&nbsp;<a class='btn btn-primary' href='?VisualizarTema=true&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&ProfesorId=".$Id_Profesor."&NivelId=".$fila['NivelId']."' role='button' style='width:10%;margin-left:40%;margin-top:-3%;'>Activar Tema</a><a class='btn btn-primary' href='?OcultarTema=true&&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&ProfesorId=".$Id_Profesor."&NivelId=".$fila['NivelId']."' role='button' style='width:15%;margin-left:80%;margin-top:-3%;'>Desactivar Tema</a>
 						</li>");
 					echo("<div class='row'>
 							  <div class='col'>
@@ -42,7 +42,7 @@
 									$nfilasPuntos=mysqli_num_rows($Puntos);
 									if($nfilasPuntos>0){
 										while($filaPuntos=mysqli_fetch_array($Puntos)){
-											echo("<div class='bg-secondary' style='width:40%;text-align:center;'><h5>".$filaPuntos['Titulo']."</h5></div><a class='btn btn-primary' href='?OcultarPunto=true&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&PuntoId=".$filaPuntos['PuntoId']."' role='button' style='width:10%;margin-left:50%;margin-top:-3%;'>Ocultar Punto</a> <a class='btn btn-primary' href='?VisualizarPunto=true&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&PuntoId=".$filaPuntos['PuntoId']." 'role='button' style='width:10%;margin-left:70%;margin-top:-3%;'>Ver Punto</a>");
+											echo("<div class='bg-secondary' style='width:40%;text-align:center;'><h5>".$filaPuntos['Titulo']."</h5></div><a class='btn btn-primary' href='?OcultarPunto=true&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&PuntoId=".$filaPuntos['PuntoId']."' role='button' style='width:20%;margin-left:50%;margin-top:-3%;'>Desactivar Punto</a> <a class='btn btn-primary' href='?VisualizarPunto=true&Id_Activo=$Id_Materia&Id_Tema=".$fila['TemaId']."&PuntoId=".$filaPuntos['PuntoId']." 'role='button' style='width:10%;margin-left:80%;margin-top:-3%;'>Activar Punto</a>");
 											echo("<br><br>");
 											echo("<div>".$filaPuntos['texto']."</div>");
 										}
@@ -66,14 +66,30 @@
 		$Id_Materia=$_GET["Id_Activo"];
 		$Cambiar_Estado="update puntos set EsActivo=false where TemaId=".$_GET['Id_Tema']." and PuntoId=".$_GET['PuntoId'];
 		$Desactivar=mysqli_query($conexion,$Cambiar_Estado);
-		echo($_GET['Id_Tema']."&nbsp;&nbsp;".$_GET['PuntoId']);
+		// echo($_GET['Id_Tema']."&nbsp;&nbsp;".$_GET['PuntoId']);
 	}
 	
 	if(isset($_GET['VisualizarPunto'])){
 		$Id_Materia=$_GET["Id_Activo"];
 		$Cambiar_Estado="update puntos set EsActivo=true where TemaId=".$_GET['Id_Tema']." and PuntoId=".$_GET['PuntoId'];
 		$Desactivar=mysqli_query($conexion,$Cambiar_Estado);
-		echo($_GET['Id_Tema']."&nbsp;&nbsp;".$_GET['PuntoId']);
+		// echo($_GET['Id_Tema']."&nbsp;&nbsp;".$_GET['PuntoId']);
+	}
+	
+	if(isset($_GET['VisualizarTema'])){
+		$Id_Materia=$_GET["Id_Activo"];
+		// echo("Id del Tema ".$_GET['Id_Tema']."&nbsp;&nbsp; Id del Profesor ".$_GET['ProfesorId']."&nbsp;&nbsp; Id del Nivel es ".$_GET['NivelId']."&nbsp;&nbsp Id de la Materia ".$_GET["Id_Activo"]);
+		// echo($_GET['NivelId']."&nbsp;&nbsp;".$_GET['Id_Tema']."&nbsp;&nbsp;El id del Profesor es ".$_GET['ProfesorId']);
+		$Cambiar_Estado="update temas t set EsActivo=true where (TemaId=".$_GET['Id_Tema']." and profesorId=".$_GET['ProfesorId']." and NivelId=".$_GET['NivelId']." and MateriaId=".$_GET["Id_Activo"].")";
+		$Desactivar=mysqli_query($conexion,$Cambiar_Estado);
+	}
+	
+	if(isset($_GET['OcultarTema'])){
+		$Id_Materia=$_GET["Id_Activo"];
+		// echo("Id del Tema ".$_GET['Id_Tema']."&nbsp;&nbsp; Id del Profesor ".$_GET['ProfesorId']."&nbsp;&nbsp; Id del Nivel es ".$_GET['NivelId']."&nbsp;&nbsp Id de la Materia ".$_GET["Id_Activo"]);
+		// echo($_GET['NivelId']."&nbsp;&nbsp;".$_GET['Id_Tema']."&nbsp;&nbsp;El id del Profesor es ".$_GET['ProfesorId']);
+		$Cambiar_Estado="update temas t set EsActivo=false where (TemaId=".$_GET['Id_Tema']." and profesorId=".$_GET['ProfesorId']." and NivelId=".$_GET['NivelId']." and MateriaId=".$_GET["Id_Activo"].")";
+		$Desactivar=mysqli_query($conexion,$Cambiar_Estado);
 	}
 	?>
 	</body>
