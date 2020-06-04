343c96314adce7e0b9f335c5973a24a190d7e4b0
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-04 09:45:14 +0200

    Create PuntosTema.java

---
+++ AulaVirtualSpring/src/main/java/org/example/Entities/PuntosTema.java
@@ -0,0 +1,10 @@
+package org.example.Entities;
+
+public class PuntosTema {
+    private int IdPunto;
+    private int TemaId;
+    private String TituloPunto;
+    private String ResumenPunto;
+    private String TextoPunto;
+    private boolean EsActivoPunto;
+}
