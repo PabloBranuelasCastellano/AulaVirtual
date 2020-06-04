b76d546983278bd1db9b704b6276e288de1862c3
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-04 13:03:03 +0200

    Update LogginController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/LogginController.java
@@ -43,6 +43,7 @@ public class LogginController {
     public String ComprobarUsuario(HttpServletRequest request, Model model) throws SQLException {
             loginServices.ComprobarUsuario(request,model);
             if(loginServices.getRol()==null) {
+                model.addAttribute("Fallo",loginServices.getRol());
                 return "Errores";
 
             }
