616ede04a2434f6c1c16fd71546b9bf5934b660e
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-04 10:01:39 +0200

    Update PuntosTema.java

--- AulaVirtualSpring/src/main/java/org/example/Entities/PuntosTema.java
+++ AulaVirtualSpring/src/main/java/org/example/Entities/PuntosTema.java
@@ -6,5 +6,44 @@ public class PuntosTema {
     private String TituloPunto;
     private String ResumenPunto;
     private String TextoPunto;
-    private boolean EsActivoPunto;
+
+    public int getIdPunto() {
+        return IdPunto;
+    }
+
+    public void setIdPunto(int idPunto) {
+        IdPunto = idPunto;
+    }
+
+    public int getTemaId() {
+        return TemaId;
+    }
+
+    public void setTemaId(int temaId) {
+        TemaId = temaId;
+    }
+
+    public String getTituloPunto() {
+        return TituloPunto;
+    }
+
+    public void setTituloPunto(String tituloPunto) {
+        TituloPunto = tituloPunto;
+    }
+
+    public String getResumenPunto() {
+        return ResumenPunto;
+    }
+
+    public void setResumenPunto(String resumenPunto) {
+        ResumenPunto = resumenPunto;
+    }
+
+    public String getTextoPunto() {
+        return TextoPunto;
+    }
+
+    public void setTextoPunto(String textoPunto) {
+        TextoPunto = textoPunto;
+    }
 }
