e07b959fc686b7243f01351f2a3821e8d4f08be2
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-08 12:42:06 +0200

    Update AlumnosServices.java

--- AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
+++ AulaVirtualSpring/src/main/java/org/example/services/AlumnosServices.java
@@ -3,6 +3,7 @@ package org.example.services;
 import org.example.Entities.Alumnos;
 import org.example.Entities.GruposAlumno;
 import org.example.Entities.Materias;
+import org.example.Entities.Temas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -24,6 +25,7 @@ public class AlumnosServices {
     Alumnos alumnos;
     GruposAlumno gruposAlumno;
     Materias materias;
+    Temas temas;
     @Autowired
     DataSource dataSource=null;
     Connection connection=null;
@@ -79,5 +81,34 @@ public class AlumnosServices {
         }
         model.addAttribute("MateriaAlumno",materiasList);
         return "panelalumnos";
+    }
+    public String TemasAlumnos(HttpServletRequest request, Model model, int ProfesorId,int MateriaId,int NivelId) throws SQLException {
+        connection = dataSource.getConnection();
+        String Ver_Temas = "select distinct t.MateriaId,t.profesorId,t.titulo,m.Nombre,t.TemaId,t.NivelId ,t.EsActivo from temas t, profesores p ,niveles n,materias m where(t.materiaId=m.MateriaId and n.nivelId=t.nivelId and t.profesorId=? and t.materiaId=? and t.EsActivo=true)";
+        PreparedStatement preparedStatement = connection.prepareStatement(Ver_Temas);
+        preparedStatement.setInt(1,materias.getProfesorId());
+        preparedStatement.setInt(2, MateriaId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Temas> temasList = new ArrayList<>();
+        while (resultSet.next()) {
+            temas = new Temas();
+            temas.setTemaId(resultSet.getInt("TemaId"));
+            //System.out.println("Id del Tema "+resultSet.getInt("TemaId"));
+            temas.setTituloTema(resultSet.getString("Titulo"));
+            //System.out.println("Titulo del Tema "+resultSet.getString("Titulo"));
+            temas.setProfesorId(resultSet.getInt("ProfesorId"));
+            //System.out.println("Id del Profesor "+resultSet.getInt("ProfesorId"));
+            temas.setNivelId(resultSet.getInt("NivelId"));
+            //System.out.println("El id del Nivel es "+resultSet.getInt("NivelId"));
+            temas.setMateriaId(resultSet.getInt("MateriaId"));
+            //System.out.println("El id de la Materia es "+resultSet.getInt("MateriaId"));
+            temas.setTemaActivo(resultSet.getBoolean("EsActivo"));
+            //System.out.println("Esta el tema activado? "+resultSet.getBoolean("EsActivo"));
+            temasList.add(temas);
+            model.addAttribute("ListaTemas", temasList);
+
+
+        }
+        return "Desplegar_Temas";
     }
 }
