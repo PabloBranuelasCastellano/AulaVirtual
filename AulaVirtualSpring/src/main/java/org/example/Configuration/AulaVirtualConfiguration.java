05894f53e5500a14eb8f04abb2315a5c499403d8
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-09 09:35:24 +0200

    Update AulaVirtualConfiguration.java

--- AulaVirtualSpring/src/main/java/org/example/Configuration/AulaVirtualConfiguration.java
+++ AulaVirtualSpring/src/main/java/org/example/Configuration/AulaVirtualConfiguration.java
@@ -1,8 +1,28 @@
 package org.example.Configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AulaVirtualConfiguration {
 
+    @Bean
+    public OpenAPI customOpenApi(){
+        Contact contact=new Contact();
+        contact.setEmail("pablo.branuelas@campusfp.es");
+        contact.setName("Pablo ");
+
+
+        return new OpenAPI()
+            .components(new Components())
+            .info(new Info()
+            .title("Biblioteca Spring")
+            .contact(contact)
+            .version("1.0")
+            .description("Biblioteca Spring con Microservicios"));
+    }
 }
