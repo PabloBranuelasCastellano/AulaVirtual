40995b1dfff69cdeeb0f891c31b201f71a098622
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-10 14:50:49 +0200

    Update Cuestionarios.java

--- AulaVirtualSpring/src/main/java/org/example/Entities/Cuestionarios.java
+++ AulaVirtualSpring/src/main/java/org/example/Entities/Cuestionarios.java
@@ -1,4 +1,67 @@
 package org.example.Entities;
 
 public class Cuestionarios {
+    private int ProfesorId;
+    private String Nombre_Examen;
+    private String Resumen_Examen;
+    private String Instrucciones_Examen;
+    private float PuntosAcierto;
+    private float PuntosError;
+    private int Num_Preguntas;
+
+    public int getProfesorId() {
+        return ProfesorId;
+    }
+
+    public void setProfesorId(int profesorId) {
+        ProfesorId = profesorId;
+    }
+
+    public String getNombre_Examen() {
+        return Nombre_Examen;
+    }
+
+    public void setNombre_Examen(String nombre_Examen) {
+        Nombre_Examen = nombre_Examen;
+    }
+
+    public String getResumen_Examen() {
+        return Resumen_Examen;
+    }
+
+    public void setResumen_Examen(String resumen_Examen) {
+        Resumen_Examen = resumen_Examen;
+    }
+
+    public String getInstrucciones_Examen() {
+        return Instrucciones_Examen;
+    }
+
+    public void setInstrucciones_Examen(String instrucciones_Examen) {
+        Instrucciones_Examen = instrucciones_Examen;
+    }
+
+    public float getPuntosAcierto() {
+        return PuntosAcierto;
+    }
+
+    public void setPuntosAcierto(float puntosAcierto) {
+        PuntosAcierto = puntosAcierto;
+    }
+
+    public float getPuntosError() {
+        return PuntosError;
+    }
+
+    public void setPuntosError(float puntosError) {
+        PuntosError = puntosError;
+    }
+
+    public int getNum_Preguntas() {
+        return Num_Preguntas;
+    }
+
+    public void setNum_Preguntas(int num_Preguntas) {
+        Num_Preguntas = num_Preguntas;
+    }
 }
