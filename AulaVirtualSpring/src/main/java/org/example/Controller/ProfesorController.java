d066a3f8f006f06210c6c90532387c3477c3aad2
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-16 18:43:42 +0200

    Update ProfesorController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -124,4 +124,13 @@ public class ProfesorController {
     public String VerPreguntas(HttpServletRequest request,Model model,@PathVariable int ExamenId)throws SQLException{
         return profesoresServices.VerPreguntas(request,model,ExamenId);
     }
+    @GetMapping("ActivarPregunta/{PreguntaId}/{PreguntaId}/{CuestionarioId}")
+    public String ActivarPregunta(HttpServletRequest request,Model model,@PathVariable int PreguntaId,@PathVariable int CuestionarioId)throws SQLException{
+        return profesoresServices.ActivarPregunta(request,model,PreguntaId,CuestionarioId);
+    }
+
+    @GetMapping("PanelPreguntas/DesactivarPregunta/{PreguntaId}/{CuestionarioId}")
+    public String DesactivarPregunta(HttpServletRequest request,Model model,@PathVariable int PreguntaId,@PathVariable int CuestionarioId)throws SQLException{
+        return profesoresServices.DesactivarPregunta(request,model,PreguntaId,CuestionarioId);
+    }
 }
