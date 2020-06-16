6ff0d41f8d946daf38cf633e2f18df765c20dd95
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-06-16 18:08:02 +0200

    Update ProfesorController.java

--- AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
+++ AulaVirtualSpring/src/main/java/org/example/Controller/ProfesorController.java
@@ -120,8 +120,8 @@ public class ProfesorController {
         return profesoresServices.CrearPreguntas(request,model);
     }
 
-    @GetMapping("PanelPreguntas/")
-    public String VerPreguntas(HttpServletRequest request,Model model)throws SQLException{
-        return profesoresServices.VerPreguntas(request,model);
+    @GetMapping("PanelPreguntas/{ExamenId}")
+    public String VerPreguntas(HttpServletRequest request,Model model,@PathVariable int ExamenId)throws SQLException{
+        return profesoresServices.VerPreguntas(request,model,ExamenId);
     }
 }
